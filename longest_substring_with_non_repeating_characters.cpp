6d1e321c884c19dcf2d7689956c97c8432447d6b
Vivekbelwal <38004314+Vivekbelwal@users.noreply.github.com>
2020-06-09 11:44:51 +0530

    Add files via upload

---
+++ longest_substring_with_non_repeating_characters.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int length ( string str ){
+    int n = str.size();
+    int res =0;
+    vector <int> lastIndex(256,-1);
+    
+    int i = 0;
+    for ( int j {} ; j<n ; j++ ){
+        
+        i = max( i , lastIndex[str[j]]+1 );
+        
+        res = max ( res , j-i+1 );
+        
+        lastIndex[str[j]]=j;
+    }
+    return res;
+}
+
+
+int main(){
+    string str {"abcabcabc"};
+    cout << length(str) << endl;
+}
